d438a00917fbdc52320ed7fc270187bb22a91537
Sahil Mehraj <sahil007mehraj@gmail.com>
2025-10-20 16:41:36 +0530

    feat: Add Timesheet and org chart

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
 import EmployeeManagement from "./pages/EmployeeManagement";
+import Timesheet from "./pages/Timesheet";
+import Orgchart from "./pages/Orgchart";
 
 function App() {
   return (
     <div className="flex min-h-screen">
       {/* Sidebar */}
-      <div className="hidden md:flex md:w-64">
+      <div className="hidden md:flex md:w-64 z-[100]">
         <Sidebar />
       </div>
 
@@ -21,18 +23,19 @@ function App() {
           <Navbar />
         </div>
 
-        <main className="px-4 py-6">
+        <main className="px-4 py-6 flex-1">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/employee-management" element={<EmployeeManagement/>}/>
+            <Route path="/timesheets" element={<Timesheet/>}/>
+            {/* 🎯 UPDATED ROUTE: Changed from /organisation-chart to /orgchart */}
+            <Route path="/orgchart" element={<Orgchart/>}/> 
           </Routes>
         </main>
-
         <Footer />
       </div>
     </div>
-
   );
 }
 
